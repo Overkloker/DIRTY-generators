cb1670b0090a0ae72f6d784a159b2b9c4b5ab2d4
Bogdan Tkachuk <bogdantk1@gmail.com>
2015-10-31 13:47:03 +0200

    Added names library

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
 <?php
-echo 'hello';
+header('Content-type: text/html; charset=utf-8');
+require 'Names/NCL.NameCase.ua.php';
+
+$vidm = strtolower($_GET['v']);
+$name = $_GET['n'];
+
+$v = [
+    'називний' => 0,
+    'родовий' => 1,
+    'давальний' => 2,
+    'знахідний' => 3,
+    'орудний' => 4,
+    'місцевий' => 5,
+    'кличний' => 6
+];
+
+$nc = new NCLNameCaseUa();
+$text = 'Я, {me_n} урочисто заявляю що {me_z}';
+echo $nc->q($name)[$v[$vidm]];
